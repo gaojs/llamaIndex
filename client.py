7b386d18f96375cd7fd927cb542705486733daa8
hill.gao <hill.gao@yuansuan.com>
2025-01-08 16:00:11 +0800

    将“添加知识库”放到tab列表最前面，以规避BUG

--- client.py
+++ client.py
@@ -18,12 +18,12 @@ with gr.Blocks() as demo:
         if not new_tab_name.strip():  # 检查名称是否为空
             return tables, new_tab_name
         # 返回新的标签列表，并清空输入框
-        return [{"name": new_tab_name}]+tables, ""
+        return tables+[{"name": new_tab_name}], ""
 
     # 上传文件
     def upload_files(files, new_tab_name):
         if not new_tab_name.strip():  # 如果名称为空（去除前后空格后）
-            return gr.Text(value="**警告：** 请先输入新知识库的名称！", visible=True)
+            return gr.Text(value="警告：请先输入新知识库的名称，再上传文件！", visible=True)
         path = "./data/" + new_tab_name + "/"
         folder = os.path.exists(path)
         if not folder:
@@ -36,8 +36,34 @@ with gr.Blocks() as demo:
     def load_tab_data(tab_name):
         return tabs_data.get(tab_name, [])
 
+    # 处理用户输入，发送请求并更新聊天记录
+    def request(message, chat_history, cur_tab):
+        url = "http://127.0.0.1:5678/query"
+        params = {'name': cur_tab, 'text': message}
+        # 发送 GET 请求
+        response = requests.get(url, params=params)
+        # 检查请求状态
+        if response.status_code == 200:
+            response_msg = response.text
+        else:
+            response_msg = "请求失败"
+        chat_history.append(gr.ChatMessage(role="user", content=message))
+        chat_history.append(gr.ChatMessage(role="assistant", content=response_msg))
+        tabs_data[cur_tab] = chat_history
+        # 清空输入框，并返回新的聊天记录
+        return "", chat_history
+
     @gr.render(inputs=tabs)
     def render_todos(tabs_list):
+        with gr.Tab("添加知识库"):
+            with gr.Column():
+                tab = gr.Textbox(label="输入新知识库的名称：", placeholder="请输入新知识库标题")
+                files = gr.Files(label="上传知识库文件")
+                button = gr.Button("创建知识库")
+                alert = gr.Text(visible=False)  # 用于显示警告信息
+            files.upload(upload_files, [files, tab], [alert])
+            button.click(add_tabs, [tabs, tab], [tabs, tab])
+
         # 循环遍历tabs_list
         for tab in tabs_list:
             tab_name = tab["name"]
@@ -47,36 +73,8 @@ with gr.Blocks() as demo:
                                      latex_delimiters=[{"left": "$$", "right": "$$", "display": True},
                                                        {"left": "$", "right": "$", "display": False}])
                 msg = gr.Textbox(show_label=False, placeholder="输入消息...")  # 输入文本框
-
-                # 处理用户输入，发送请求并更新聊天记录
-                def request(message, chat_history, cur_tab):
-                    url = "http://127.0.0.1:5678/query"
-                    params = {'name': cur_tab, 'text': message}
-                    # 发送 GET 请求
-                    response = requests.get(url, params=params)
-                    # 检查请求状态
-                    if response.status_code == 200:
-                        response_msg = response.text
-                    else:
-                        response_msg = "请求失败"
-                    chat_history.append(gr.ChatMessage(role="user", content=message))
-                    chat_history.append(gr.ChatMessage(role="assistant", content=response_msg))
-                    tabs_data[cur_tab] = chat_history
-                    # 清空输入框，并返回新的聊天记录
-                    return "", chat_history
-
                 gtab.select(lambda name=tab_name: (name, load_tab_data(name)), None, [state_tab, chatbot])
-
             msg.submit(request, [msg, chatbot, state_tab], [msg, chatbot])
-
-        with gr.Tab("添加知识库"):
-            with gr.Column():
-                tab = gr.Textbox(label="输入新知识库的名称：", placeholder="请输入新知识库标题")
-                files = gr.Files(label="上传知识库文件")
-                button = gr.Button("创建知识库")
-                alert = gr.Text(visible=False)  # 用于显示警告信息
-            files.upload(upload_files, [files, tab], [alert])
-            button.click(add_tabs, [tabs, tab], [tabs, tab])
 
 
 if __name__ == "__main__":
